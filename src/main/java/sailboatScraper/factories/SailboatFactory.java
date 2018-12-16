b6b841aedd9c6787a255ec8d3f1d742c97cf7690
Christopher Oliver <coliver650@gmail.com>
2018-12-16 00:02:59 -0600

    Begin html parsing.

--- src/main/java/sailboatScraper/factories/SailboatFactory.java
+++
@@ -1,7 +0,0 @@
-package sailboatScraper.factories;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class SailboatFactory {
-}
--- src/main/java/sailboatScraper/services/implementations/SailboatScrappingServiceImpl.java
+++ src/main/java/sailboatScraper/services/implementations/SailboatScrappingServiceImpl.java
@@ -1,13 +1,15 @@
 package sailboatScraper.services.implementations;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 import sailboatScraper.entities.Sailboat;
-import sailboatScraper.factories.SailboatFactory;
 import sailboatScraper.services.ScrappingService;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,49 +17,103 @@ import java.util.List;
 @Service
 public class SailboatScrappingServiceImpl implements ScrappingService {
 
-    private final RestTemplate restTemplate;
-    private final SailboatFactory sailboatFactory;
-
-    public SailboatScrappingServiceImpl(RestTemplate restTemplate, SailboatFactory sailboatFactory) {
-
-        this.restTemplate = restTemplate;
-        this.sailboatFactory = sailboatFactory;
-    }
-
     @Override
     public List<Sailboat> scrapeSite() {
 
         List<Sailboat> sailboats = new ArrayList<>();
+        Document document;
 
-        String html = restTemplate.getForObject("https://www.yachtworld.com/core/listing/cache/searchResults.jsp?cit=true&slim=quick&ybw=&sm=3&searchtype=advancedsearch&Ntk=boatsEN&Ntt=&is=false&man=&hmid=0&ftid=0&enid=0&type=%28Sail%29&fromLength=38&toLength=50&fromYear=2013&toYear=2019&fromPrice=100000&toPrice=300000&luom=126&currencyid=100&city=&pbsint=&boatsAddedSelected=-1No=0&ps=10", String.class);
-
-        if (html == null) {
+        try {
+            document = Jsoup.connect("https://www.yachtworld.com/core/listing/cache/searchResults.jsp?cit=true&slim=quick&sm=3&searchtype=advancedsearch&Ntk=boatsEN&hmid=0&ftid=0&enid=0&type=Sail&fromLength=38&toLength=45&fromYear=2013&toYear=2019&fromPrice=100000&toPrice=300000&luom=126&currencyid=100&boatsAddedSelected=-1&fracts=1").get();
+        } catch (IOException ioe) {
             return sailboats;
         }
 
-        Document document = Jsoup.parse(html);
+        Elements listings = document.getElementsByClass("information");
 
-        sailboats.add(
-                new Sailboat(
-                        6241053L,
-                        48,
-                        2009,
-                        "Jeanneau",
-                        "Sun Odyssey 50 DS",
-                        202413,
-                        html,
-                        LocalDateTime.now()));
-        sailboats.add(
-                new Sailboat(
-                        6744711L,
-                        48,
-                        2008,
-                        "Jeanneau",
-                        "Sun Odyssey 50 DS",
-                        220506,
-                        "Barcelona, Spain",
-                        LocalDateTime.now()));
+        for (Element listing : listings) {
+
+            Elements makeModelElements = listing.getElementsByClass("make-model");
+
+            if (makeModelElements.isEmpty()) {
+                continue;
+            }
+
+            Elements priceElements = listing.getElementsByClass("price");
+
+            if (priceElements.isEmpty()) {
+                continue;
+            }
+
+            Elements locationElements = listing.getElementsByClass("location");
+
+            if (locationElements.isEmpty()) {
+                continue;
+            }
+
+            sailboats.add(
+                    new Sailboat(
+                            getId(makeModelElements.get(0)),
+                            getLength(makeModelElements.get(0)),
+                            getYear(makeModelElements.get(0)),
+                            getMake(makeModelElements.get(0)),
+                            getModel(makeModelElements.get(0)),
+                            getPrice(priceElements.get(0)),
+                            getLocation(locationElements.get(0)),
+                            LocalDateTime.now()));
+        }
 
         return sailboats;
+    }
+
+    private long getId(Element makeModelElement) {
+
+        String linkHref = makeModelElement.selectFirst("a").attr("href");
+        String[] linkNodes = linkHref.split("-");
+        String rawId = linkNodes[linkNodes.length - 1];
+        String id = rawId.substring(0, rawId.length() - 1);
+
+        return Long.valueOf(id);
+    }
+
+    private float getLength(Element makeModelElement) {
+
+        String rawLength = makeModelElement.getElementsByClass("length feet").get(0).text();
+        String[] rawLengthParts = rawLength.split(" ");
+
+        return Integer.valueOf(rawLengthParts[0]);
+    }
+
+    private int getYear(Element makeModelElement) {
+
+        String linkText = makeModelElement.selectFirst("a").text();
+        String[] linkTextNodes = linkText.split(" ");
+
+        return Integer.valueOf(linkTextNodes[2]);
+    }
+
+    private String getMake(Element makeModelElement) {
+
+        String linkText = makeModelElement.selectFirst("a").text();
+        String[] linkTextNodes = linkText.split(" ");
+
+        return linkTextNodes[3];
+    }
+
+    private String getModel(Element makeModelElement) {
+
+        String linkText = makeModelElement.selectFirst("a").text();
+        int index = StringUtils.ordinalIndexOf(linkText, " ", 4);
+
+        return linkText.substring(index + 1);
+    }
+
+    private float getPrice(Element priceElement) {
+        return 202413;
+    }
+
+    private String getLocation(Element locationElement) {
+
+        return locationElement.text();
     }
 }
