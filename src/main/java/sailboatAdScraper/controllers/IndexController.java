1a65ab94eb857690b12b4fc0b19390aa43ff6d6b
Christopher Oliver <coliver650@gmail.com>
2018-12-22 14:29:50 -0600

    Add simple, concurrent hashmap caching.

--- src/main/java/sailboatAdScraper/controllers/IndexController.java
+++ src/main/java/sailboatAdScraper/controllers/IndexController.java
@@ -2,22 +2,21 @@ package sailboatAdScraper.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import sailboatAdScraper.domain.services.implementations.SailboatAdScrapingServiceImpl;
-import sailboatAdScraper.domain.valueObjects.SailboatAd;
+import sailboatAdScraper.domain.services.ScrapingService;
 
 import java.util.List;
 
 @RestController
 public class IndexController {
 
-    private final SailboatAdScrapingServiceImpl sailboatAdScrapingService;
+    private final ScrapingService sailboatAdScrapingService;
 
-    public IndexController(SailboatAdScrapingServiceImpl sailboatAdScrapingService) {
+    public IndexController(ScrapingService sailboatAdScrapingService) {
         this.sailboatAdScrapingService = sailboatAdScrapingService;
     }
 
     @RequestMapping("/")
-    public List<SailboatAd> getSailboatAds() {
+    public List<?> getSailboatAds() {
         return sailboatAdScrapingService.scrapeSite();
     }
 }
--- src/main/java/sailboatAdScraper/domain/services/implementations/SailboatAdScrapingServiceImpl.java
+++ src/main/java/sailboatAdScraper/domain/services/implementations/SailboatAdScrapingServiceImpl.java
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import sailboatAdScraper.domain.valueObjects.SailboatAd;
 import sailboatAdScraper.domain.services.ScrapingService;
@@ -18,6 +19,7 @@ import java.util.List;
 public class SailboatAdScrapingServiceImpl implements ScrapingService {
 
     @Override
+    @Cacheable("sailboatAds")
     public List<SailboatAd> scrapeSite() {
 
         List<SailboatAd> sailboatAds = new ArrayList<>();
