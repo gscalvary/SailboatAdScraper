1be98163ee49deebed0e81a8d4c63d804875b2b1
Christopher Oliver <coliver650@gmail.com>
2019-01-13 14:21:04 -0600

    add test class skeleton

--- src/test/groovy/sailboatAdScraper/domain/services/SailboatAdScrapingServiceSpec.groovy
+++ src/test/groovy/sailboatAdScraper/domain/services/SailboatAdScrapingServiceSpec.groovy
@@ -4,4 +4,19 @@ import spock.lang.Specification
 
 class SailboatAdScrapingServiceSpec extends Specification {
 
+    def 'A sailboat ad without a sailboat make and model should be discarded from output' () {
+        assert true
+    }
+
+    def 'A sailboat ad without a sailboat price should be discarded from output' () {
+        assert true
+    }
+
+    def 'A sailboat ad without a sailboat location should be discarded from output' () {
+        assert true
+    }
+
+    def 'A sailboat ad with make and model, price, and location should be included in output' () {
+        assert true
+    }
 }
