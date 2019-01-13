b6ae0388dca6fd1c4fb16c314f6d85ad8240892d
Christopher Oliver <coliver650@gmail.com>
2019-01-13 17:17:10 -0600

    Save work.

--- src/main/java/sailboatAdScraper/domain/services/SailboatAdScrapingService.java
+++ src/main/java/sailboatAdScraper/domain/services/SailboatAdScrapingService.java
@@ -23,40 +23,7 @@ public class SailboatAdScrapingService {
     public List<SailboatAd> scrapeSite() {
 
         List<SailboatAd> sailboatAds = new ArrayList<>();
-        Document document;
-        Map<String, String> requestParameters = new HashMap<>();
-
-        requestParameters.put("cit", "true");
-        requestParameters.put("slim", "quick");
-        requestParameters.put("sm", "3");
-        requestParameters.put("searchtype", "advancedsearch");
-        requestParameters.put("Ntk", "boatsEN");
-        requestParameters.put("hmid", "0");
-        requestParameters.put("ftid", "0");
-        requestParameters.put("enid", "0");
-        requestParameters.put("type", "Sail");
-        requestParameters.put("fromLength", "31");
-        requestParameters.put("toLength", "51");
-        requestParameters.put("fromYear", "2013");
-        requestParameters.put("toYear", "2019");
-        requestParameters.put("fromPrice", "50000");
-        requestParameters.put("toPrice", "400000");
-        requestParameters.put("luom", "126");
-        requestParameters.put("currencyid", "100");
-        requestParameters.put("boatsAddedSelected", "-1");
-        requestParameters.put("fracts", "1");
-        requestParameters.put("No", "0");
-        requestParameters.put("ps", "2000");
-
-        try {
-            document = Jsoup.connect("https://www.yachtworld.com/core/listing/cache/searchResults.jsp")
-                    .data(requestParameters)
-                    .timeout(60000)
-                    .maxBodySize(0)
-                    .get();
-        } catch (IOException ioe) {
-            return sailboatAds;
-        }
+        Document document = getDocument();
 
         Elements listings = document.getElementsByClass("information");
 
@@ -98,6 +65,48 @@ public class SailboatAdScrapingService {
         }
 
         return sailboatAds;
+    }
+
+    private Document getDocument() {
+
+        Document document;
+        Map<String, String> requestParameters = new HashMap<>();
+
+        String uri = "https://www.yachtworld.com/core/listing/cache/searchResults.jsp";
+
+        requestParameters.put("cit", "true");
+        requestParameters.put("slim", "quick");
+        requestParameters.put("sm", "3");
+        requestParameters.put("searchtype", "advancedsearch");
+        requestParameters.put("Ntk", "boatsEN");
+        requestParameters.put("hmid", "0");
+        requestParameters.put("ftid", "0");
+        requestParameters.put("enid", "0");
+        requestParameters.put("type", "Sail");
+        requestParameters.put("fromLength", "31");
+        requestParameters.put("toLength", "51");
+        requestParameters.put("fromYear", "2013");
+        requestParameters.put("toYear", "2019");
+        requestParameters.put("fromPrice", "50000");
+        requestParameters.put("toPrice", "400000");
+        requestParameters.put("luom", "126");
+        requestParameters.put("currencyid", "100");
+        requestParameters.put("boatsAddedSelected", "-1");
+        requestParameters.put("fracts", "1");
+        requestParameters.put("No", "0");
+        requestParameters.put("ps", "2000");
+
+        try {
+            document = Jsoup.connect(uri)
+                    .data(requestParameters)
+                    .timeout(60000)
+                    .maxBodySize(0)
+                    .get();
+        } catch (IOException ioe) {
+            document  = new Document(uri);
+        }
+
+        return document;
     }
 
     private float getLength(Element makeModelElement) {
--- src/test/groovy/sailboatAdScraper/domain/services/SailboatAdScrapingServiceSpec.groovy
+++ src/test/groovy/sailboatAdScraper/domain/services/SailboatAdScrapingServiceSpec.groovy
@@ -1,11 +1,27 @@
 package sailboatAdScraper.domain.services
 
+import org.jsoup.nodes.Document
 import spock.lang.Specification
 
 class SailboatAdScrapingServiceSpec extends Specification {
 
+    def sailboatAdScrapingService
+
+    def setup () {
+
+        sailboatAdScrapingService = new SailboatAdScrapingService()
+    }
+
     def 'A sailboat ad without a sailboat make and model should be discarded from output' () {
-        assert true
+
+        given:
+        sailboatAdScrapingService.getDocument() >> getDocumentWithoutMakeAndModel()
+
+        when:
+        def sailboatAds = sailboatAdScrapingService.scrapeSite()
+
+        then:
+        sailboatAds.isEmpty()
     }
 
     def 'A sailboat ad without a sailboat price should be discarded from output' () {
@@ -18,5 +34,11 @@ class SailboatAdScrapingServiceSpec extends Specification {
 
     def 'A sailboat ad with make and model, price, and location should be included in output' () {
         assert true
+    }
+
+    def getDocumentWithoutMakeAndModel ()
+    {
+
+        return new Document("")
     }
 }
