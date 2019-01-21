ae1fe1abea09273056dbfe14b8d4d4e64e0bd43f
Christopher Oliver <coliver650@gmail.com>
2019-01-21 14:39:41 -0600

    configuration complete for the time being

--- src/main/java/sailboatAdScraper/controllers/IndexController.java
+++ src/main/java/sailboatAdScraper/controllers/IndexController.java
@@ -1,65 +1,37 @@
 package sailboatAdScraper.controllers;
 
 import org.jsoup.nodes.Document;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sailboatAdScraper.configuration.YachtWorldProperties;
 import sailboatAdScraper.domain.factories.SailboatAdFactory;
 import sailboatAdScraper.domain.services.SiteScrapingService;
 import sailboatAdScraper.domain.valueObjects.SailboatAd;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class IndexController {
 
-    @Value("${yachtworld.url}")
-    private String url;
-
     private final SiteScrapingService siteScrapingService;
     private final SailboatAdFactory sailboatAdFactory;
+    private final YachtWorldProperties yachtWorldProperties;
 
-    public IndexController(SiteScrapingService siteScrapingService, SailboatAdFactory sailboatAdFactory) {
+    public IndexController(SiteScrapingService siteScrapingService,
+                           SailboatAdFactory sailboatAdFactory,
+                           YachtWorldProperties yachtWorldProperties) {
         this.siteScrapingService = siteScrapingService;
         this.sailboatAdFactory = sailboatAdFactory;
+        this.yachtWorldProperties = yachtWorldProperties;
     }
 
-    @GetMapping({"/", "/yachtworld"})
+    @GetMapping("/")
     public List<SailboatAd> getSailboatAds() {
 
-        Document document = siteScrapingService.scrapeSite(url, getYachtWorldRequestParameters());
+        Document document = siteScrapingService.scrapeSite(
+                yachtWorldProperties.getUrl(),
+                yachtWorldProperties.getRequestParameters());
 
         return sailboatAdFactory.getSailboatAds(document);
-    }
-
-    private Map<String, String> getYachtWorldRequestParameters() {
-
-        Map<String, String> requestParameters = new HashMap<>();
-
-        requestParameters.put("cit", "true");
-        requestParameters.put("slim", "quick");
-        requestParameters.put("sm", "3");
-        requestParameters.put("searchtype", "advancedsearch");
-        requestParameters.put("Ntk", "boatsEN");
-        requestParameters.put("hmid", "0");
-        requestParameters.put("ftid", "0");
-        requestParameters.put("enid", "0");
-        requestParameters.put("type", "Sail");
-        requestParameters.put("fromLength", "31");
-        requestParameters.put("toLength", "51");
-        requestParameters.put("fromYear", "2013");
-        requestParameters.put("toYear", "2019");
-        requestParameters.put("fromPrice", "50000");
-        requestParameters.put("toPrice", "400000");
-        requestParameters.put("luom", "126");
-        requestParameters.put("currencyid", "100");
-        requestParameters.put("boatsAddedSelected", "-1");
-        requestParameters.put("fracts", "1");
-        requestParameters.put("No", "0");
-        requestParameters.put("ps", "2000");
-
-        return requestParameters;
     }
 }
